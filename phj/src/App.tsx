d1d6103352f9a97a1038841b1c33752812e60869
zoeznm <bomikim046@gmail.com>
2025-05-18 16:51:26 +0900

    배경에 노이즈 넣기

---
+++ phj/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      {/* 이 안에 화면에 보여줄 내용을 추가하세요 */}
+    </div>
+  );
+}
--- phj/src/main.tsx
+++ phj/src/main.tsx
@@ -1,9 +1,10 @@
+// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.css';    // ← 여기에 글로벌 CSS를 불러옵니다.
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
